d358050490a248f67cc89a3507b4e346bd2c74fe
dumplingchilioil <hnahwal@gmail.com>
2026-03-17 05:01:48 +0800

    Update CartAdapter.java

--- CartAdapter.java
+++ CartAdapter.java
@@ -3,16 +3,20 @@ package com.example.myaeontogo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
-    private final ArrayList<String> items;
+    private ArrayList<CartItem> items;
+    private Runnable onCartChanged;
 
-    public CartAdapter(ArrayList<String> items) {
+    public CartAdapter(ArrayList<CartItem> items, Runnable onCartChanged) {
         this.items = items;
+        this.onCartChanged = onCartChanged;
     }
 
     @NonNull
@@ -24,19 +28,52 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.productName.setText(items.get(position));
+        CartItem item = items.get(position);
+        holder.productName.setText(item.getName());
+        holder.productPrice.setText("RM " + String.format("%.2f", item.getPrice()));
+        holder.tvQty.setText(String.valueOf(item.getQuantity()));
+
+        // CRUD: Update (Increase)
+        holder.btnPlus.setOnClickListener(v -> {
+            item.setQuantity(item.getQuantity() + 1);
+            notifyItemChanged(position);
+            onCartChanged.run();
+        });
+
+        // CRUD: Update (Decrease)
+        holder.btnMinus.setOnClickListener(v -> {
+            if (item.getQuantity() > 1) {
+                item.setQuantity(item.getQuantity() - 1);
+                notifyItemChanged(position);
+                onCartChanged.run();
+            }
+        });
+
+        // CRUD: Delete
+        holder.btnDelete.setOnClickListener(v -> {
+            items.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, items.size());
+            onCartChanged.run();
+        });
     }
 
     @Override
-    public int getItemCount() {
-        return items.size();
-    }
+    public int getItemCount() { return items.size(); }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView productName;
+        TextView productName, productPrice, tvQty;
+        ImageView btnPlus;
+        ImageButton btnMinus, btnDelete;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             productName = itemView.findViewById(R.id.productName);
+            productPrice = itemView.findViewById(R.id.productPrice);
+            tvQty = itemView.findViewById(R.id.tvQty);
+            btnPlus = itemView.findViewById(R.id.btnPlus);
+            btnMinus = itemView.findViewById(R.id.btnMinus);
+            btnDelete = itemView.findViewById(R.id.imageButton2);
         }
     }
 }
