95662df6402848e0277d4c0762c635c2e6eb9cc4
sujinasree <sujina0427@gmail.com>
2026-03-17 00:24:05 +0800

    Product.java

---
+++ Product.java
@@ -0,0 +1,22 @@
+package com.example.myaeonapp;
+
+public class Product {
+
+    String name;
+    String category;
+    String expiryDate;
+    int image;
+
+    public Product(String name, String category, String expiryDate, int image) {
+        this.name = name;
+        this.category = category;
+        this.expiryDate = expiryDate;
+        this.image = image;
+    }
+
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public String getExpiryDate() { return expiryDate; }
+    public int getImage() { return image; }
+
+}
