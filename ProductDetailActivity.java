06fae092e1f1042f2451d080e56763b1a569c246
sujinasree <sujina0427@gmail.com>
2026-03-17 17:50:38 +0800

    ProductDetailActivity.java

--- ProductDetailActivity.java
+++ ProductDetailActivity.java
@@ -1,6 +1,7 @@
 package com.myaeon2go;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     ImageView image;
     TextView name, expiry;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,14 @@ public class ProductDetailActivity extends AppCompatActivity {
         image = findViewById(R.id.detailImage);
         name = findViewById(R.id.detailName);
         expiry = findViewById(R.id.detailExpiry);
+        btnBack = findViewById(R.id.btnBack);
 
         name.setText(getIntent().getStringExtra("name"));
         expiry.setText("Expiry Date: " + getIntent().getStringExtra("expiry"));
         image.setImageResource(getIntent().getIntExtra("image",0));
+
+        btnBack.setOnClickListener(v -> {
+            finish(); 
+        });
     }
 }
