f0ec73926a4b7cdc27b557631f4922a1dc40ea20
dumplingchilioil <hnahwal@gmail.com>
2026-03-19 02:54:08 +0800

    Update CartItem.java

--- CartItem.java
+++ CartItem.java
@@ -1,1 +1,18 @@
+package com.example.myaeontogo;
 
+public class CartItem {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public CartItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
