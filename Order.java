ed2381457273f1824140692ac0e34c8c69b916b3
perarasi1206 <perarasi1206@gmail.com>
2026-03-19 15:05:50 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,1 +1,20 @@
+package com.myaeon2go;
 
+public class Order {
+    private final String orderId;
+    private final String date;
+    private final String amount;
+    private final String status;
+
+    public Order(String orderId, String date, String amount, String status) {
+        this.orderId = orderId;
+        this.date = date;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public String getOrderId() { return orderId; }
+    public String getDate() { return date; }
+    public String getAmount() { return amount; }
+    public String getStatus() { return status; }
+}
